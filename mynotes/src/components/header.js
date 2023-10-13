2d86eef7ea0de5f44cbf07dfdad083b18441be3e
shyamn17 <shyamn2001@gmail.com>
2023-10-13 17:39:11 +0530

    v1.1

---
+++ mynotes/src/components/header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <div className="app-header">
+            <h1>SAK Notes</h1>
+            <h3 style={{color:"white"}}>Practice <br/> Project</h3>
+        </div>
+    )
+}
+
+export default Header
